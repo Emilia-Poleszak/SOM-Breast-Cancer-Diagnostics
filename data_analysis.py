c44edf32ecdb508e775a553e709f49d6a7366e4e
Emilia <01178499@pw.edu.pl>
2025-04-10 00:46:24 +0200

    Data analysis optimization, adding requirements and histograms.

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,6 @@
-import statistics as st
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 data = pd.read_csv(r'Breast Cancer Coimbra_MLR\dataR2.csv')
 
@@ -9,23 +9,38 @@ y = data.iloc[:, -1].values
 
 n = np.count_nonzero(y==1)
 
-means1 = []
-std1 = []
-means2 = []
-std2 = []
+X1 = pd.DataFrame({'age': X[:n,0],
+                   'bmi': X[:n,1],
+                   'glucose': X[:n,2],
+                   'insulin': X[:n,3],
+                   'homa': X[:n,4],
+                   'leptin': X[:n,5],
+                   'adiponectin': X[:n,6],
+                   'resistin': X[:n,7],
+                   'mcp.1': X[:n,8]})
 
-for i in range(len(X[0])-1):
-    means1.append(st.mean(X[:-n,i]))
-    means2.append(st.mean(X[-n:,i]))
-    std1.append(st.stdev(X[:-n,i]))
-    std2.append(st.stdev(X[-n:,i]))
+X2 = pd.DataFrame({'age': X[n:,0],
+                   'bmi': X[n:,1],
+                   'glucose': X[n:,2],
+                   'insulin': X[n:,3],
+                   'homa': X[n:,4],
+                   'leptin': X[n:,5],
+                   'adiponectin': X[n:,6],
+                   'resistin': X[n:,7],
+                   'mcp.1': X[n:,8]})
 
-print("Klasa 1")
-for i in range(len(means1)):
-    print(means1[i])
-    print(std1[i])
+mean1 = X1.mean()
+mean2 = X2.mean()
+std1 = X1.std()
+std2 = X2.std()
 
-print("Klasa 2")
-for i in range(len(means2)):
-    print(means2[i])
-    print(std2[i])
+print('Class 1:\nmean values:\n', mean1, '\nstandard deviation:\n', std1,
+      '\nClass 2:\nmean values:\n', mean2, '\nstandard deviation:\n', std2)
+
+X1.hist(bins=10, figsize=(10, 6), edgecolor='black')
+plt.tight_layout()
+plt.show()
+
+X2.hist(bins=10, figsize=(10, 6), edgecolor='black')
+plt.tight_layout()
+plt.show()
