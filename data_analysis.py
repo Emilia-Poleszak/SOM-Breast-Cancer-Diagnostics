b8f2f29172a85cdc3532cb9e453193872ab0d959
Emilia <01178499@pw.edu.pl>
2025-04-10 13:40:13 +0200

    Splitting data for training and tests and adding comments.

--- data_analysis.py
+++ data_analysis.py
@@ -4,30 +4,13 @@ from matplotlib import pyplot as plt
 
 data = pd.read_csv(r'Breast Cancer Coimbra_MLR\dataR2.csv')
 
-X = data.iloc[:, :-1].values
-y = data.iloc[:, -1].values
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
 
+# splitting data by classes and statistical analysis
 n = np.count_nonzero(y==1)
-
-X1 = pd.DataFrame({'age': X[:n,0],
-                   'bmi': X[:n,1],
-                   'glucose': X[:n,2],
-                   'insulin': X[:n,3],
-                   'homa': X[:n,4],
-                   'leptin': X[:n,5],
-                   'adiponectin': X[:n,6],
-                   'resistin': X[:n,7],
-                   'mcp.1': X[:n,8]})
-
-X2 = pd.DataFrame({'age': X[n:,0],
-                   'bmi': X[n:,1],
-                   'glucose': X[n:,2],
-                   'insulin': X[n:,3],
-                   'homa': X[n:,4],
-                   'leptin': X[n:,5],
-                   'adiponectin': X[n:,6],
-                   'resistin': X[n:,7],
-                   'mcp.1': X[n:,8]})
+X1 = X.iloc[:n]
+X2 = X.iloc[n:]
 
 mean1 = X1.mean()
 mean2 = X2.mean()
@@ -37,6 +20,7 @@ std2 = X2.std()
 print('Class 1:\nmean values:\n', mean1, '\nstandard deviation:\n', std1,
       '\nClass 2:\nmean values:\n', mean2, '\nstandard deviation:\n', std2)
 
+# creating histograms
 X1.hist(bins=40, figsize=(10, 6), edgecolor='black')
 plt.tight_layout()
 plt.show()
@@ -44,3 +28,13 @@ plt.show()
 X2.hist(bins=40, figsize=(10, 6), edgecolor='black')
 plt.tight_layout()
 plt.show()
+
+# splitting data of both classes: 80% for training and 20% for tests
+rows_1_learn = int(0.8*n)
+rows_2_learn = int(0.8*(len(X)-n))
+
+X1_learn = X1.iloc[:rows_1_learn]
+X1_test = X1.iloc[rows_1_learn:]
+
+X2_learn = X2.iloc[:rows_2_learn]
+X2_test = X2.iloc[rows_2_learn:]
