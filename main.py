fe2603e40bd5be9dc18966e26de71a7e1630755e
Anna Czarkowska <01178477@pw.edu.pl>
2025-06-10 17:16:17 +0200

    parameters test - to fix: displaying confusion matrix

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-import som
+import som, metrics
 
 DATABASE_FILENAME = r'Breast Cancer Coimbra_MLR\dataR2.csv'
-EPOCHS = 1000
+EPOCHS = 3000
 NO_TRAINING_CALLS = 25
 TRAIN_TEST_RATIO = 0.75
 
+TESTED_LRS = [0.001, 0.01, 0.2, 0.8]
+TESTED_MAP_SIZES = [(3,3), (7,7), (12,12), (25,25)]
 
 def normalise_data(data: pd.DataFrame) -> pd.DataFrame:
     """
@@ -57,70 +59,8 @@ def load_data():
 
     return X_learn_data, X_test_data, y_learn_data, y_test_data
 
-def confusion_matrix(y_true, y_pred, positive_class = 2, negative_class = 1):
-    """
-    Calculates confusion matrix for given true and predicted values.
-    :param y_true: True class labels
-    :param y_pred: Predicted class labels
-    :param positive_class: Positive class label
-    :param negative_class: Negative class label
-    :return:
-    """
-    if len(y_true) != len(y_pred):
-        raise ValueError("y_true and y_pred must be of the same length.")
-
-    tp, tn, fp, fn = 0, 0, 0, 0
-    for i in range(len(y_true)):
-        if y_pred[i] == y_true[i]:
-            if y_pred[i] == positive_class:
-                tp += 1
-            elif y_pred[i] == negative_class:
-                tn += 1
-        else:
-            if y_pred[i] == positive_class:
-                fp += 1
-            if y_pred[i] == negative_class:
-                fn += 1
-    return np.array([[tp, fp], [fn, tn]])
-
-def sensitivity_specificity(test: np.array, pred: np.array) -> tuple:
-    """
-    Calculates sensitivity and specificity of given predicted labels based on true labels.
-    In true data: 1 - healthy controls (negative), 2 - patients (positive).
-    Prints results in percentage format.
-    :param test: True data labels
-    :param pred: Predicted data labels
-    :return: Percentage sensitivity and specificity
-    """
-    # test_bin = (np.array(test) == 1).astype(int)
-    # pred_bin = (np.array(pred) == 1).astype(int)
-
-    tp, fp, fn, tn = confusion_matrix(test, pred).ravel()
-
-    sensitivity = tp / (tp + fn) * 100
-    specificity = tn / (tn + fp) * 100
-
-    print("Sensitivity: {:.2f}%".format(sensitivity))
-    print("Specificity: {:.2f}%\n".format(specificity))
-    return sensitivity, specificity
-
-def accuracy(pred: np.array, test: np.array) -> float:
-    """
-    Calculates accuracy of labels predictions: number of predicted labels
-    consistent with true labels. Prints results in percentage format.
-    :param test: True data labels
-    :param pred: Predicted data labels
-    :return: Percentage of correct predictions
-    """
-    a = 0
-    for i in range(len(pred)):
-        if pred[i] == test[i]:
-            a += 1
-    a = a / len(pred) * 100
-    print("Accuracy: {:.2f}%".format(a))
-    return a
-
-def statistics(learn_x: np.array, test_x: np.array, learn_y: np.array, test_y: np.array):
+def statistics(learn_x: np.ndarray, test_x: np.ndarray,
+               learn_y: np.ndarray, test_y: np.ndarray):
     """
     Calculates and prints statistics: mean accuracy, sensitivity and specificity of SOM.
     :param learn_x: Learning data X
@@ -134,8 +74,8 @@ def statistics(learn_x: np.array, test_x: np.array, learn_y: np.array, test_y: n
     for i in range(NO_TRAINING_CALLS):
         som_clf.train(learn_x, learn_y, test_x, test_y, epochs=EPOCHS)
         pred_y = som_clf.predict(test_x)
-        acc.append(accuracy(pred_y, test_y))
-        [sens, spec] = sensitivity_specificity(test_y, pred_y)
+        acc.append(metrics.accuracy(pred_y, test_y))
+        [sens, spec] = metrics.sensitivity_specificity(test_y, pred_y)
         sensitivity.append(sens)
         specificity.append(spec)
 
@@ -151,8 +91,10 @@ def statistics(learn_x: np.array, test_x: np.array, learn_y: np.array, test_y: n
     print("Mean: {:.2f}%".format(np.mean(specificity)))
     print("Standard Deviation: {:.2f}%".format(np.std(specificity)))
 
-
-def plot_qe(x_learn_data: np.array, x_test_data: np.array, y_learn_data: np.array, y_test_data: np.array):
+def single_training(x_learn_data: np.ndarray, x_test_data: np.ndarray,
+                    y_learn_data: np.ndarray, y_test_data: np.ndarray,
+                    learning_rate: float = 0.2, std: float = 1.0,
+                    map_shape: tuple = (7, 7)):
     """
     Calculates and prints accuracy, sensitivity and specificity of SOM.
     Creates a graph of quantisation error.
@@ -160,29 +102,48 @@ def plot_qe(x_learn_data: np.array, x_test_data: np.array, y_learn_data: np.arra
     :param x_test_data: Testing data X
     :param y_learn_data: Learning data y
     :param y_test_data: Testing data y
+    :param learning_rate: Learning rate
+    :param std: Standard deviation
+    :param map_shape: Shape of map
     """
-    som_clf = som.SOMClassifier()
-    errors = som_clf.train(x_learn_data, y_learn_data, x_test_data, y_test_data, epochs=EPOCHS)
+    som_clf = som.SOMClassifier(map_shape=map_shape)
+    errors = som_clf.train(x_learn_data, y_learn_data, x_test_data, y_test_data,
+                           epochs=EPOCHS, learning_rate=learning_rate, std=std)
     y_pred = som_clf.predict(x_test_data)
 
+    print(f"\nlr: {learning_rate}, map shape: {map_shape}")
     print("True labels: \n{}".format(np.transpose(y_test_data)))
     print("Predicted labels: \n{}".format(y_pred))
 
-    accuracy(y_pred, y_test_data)
-    sensitivity_specificity(y_test_data, y_pred)
+    conf_mat = metrics.confusion_matrix(y_test_data, y_pred)
+    metrics.show_conf_matrix(conf_mat, learning_rate, map_shape)
+
+    acc = metrics.accuracy(y_pred, y_test_data)
+    print("Accuracy: {}".format(acc))
+    metrics.sensitivity_specificity(y_test_data, y_pred)
 
     fig, ax = plt.subplots()
     ax.plot([10 * i for i in range(len(errors))], errors)
+    ax.set_title(f"lr: {learning_rate}, map shape: {map_shape}")
     ax.set_xlabel("Epochs")
     ax.set_ylabel("Quantization Error")
     fig.show()
+
+def test_parameters(x_learn_data: np.ndarray, x_test_data: np.ndarray,
+                    y_learn_data: np.ndarray, y_test_data: np.ndarray):
+    for lr in TESTED_LRS:
+        for map_shape in TESTED_MAP_SIZES:
+            single_training(x_learn_data, x_test_data, y_learn_data, y_test_data,
+                            learning_rate=lr, map_shape=map_shape)
 
 
 if __name__ == '__main__':
     X_learn, X_test, y_learn, y_test = load_data()
 
     ## choose one of the functions below
-    statistics(X_learn, X_test, y_learn, y_test)
-    # plot_qe(X_learn, X_test, y_learn, y_test)
+    # statistics(X_learn, X_test, y_learn, y_test)
+    single_training(X_learn, X_test, y_learn, y_test)
+
+    # test_parameters(X_learn, X_test, y_learn, y_test)
 
     plt.show()
---
+++ metrics.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def confusion_matrix(y_true, y_pred, positive_class = 2, negative_class = 1):
+    """
+    Calculates confusion matrix for given true and predicted values.
+    :param y_true: True class labels
+    :param y_pred: Predicted class labels
+    :param positive_class: Positive class label
+    :param negative_class: Negative class label
+    :return:
+    """
+    if len(y_true) != len(y_pred):
+        raise ValueError("y_true and y_pred must be of the same length.")
+
+    tp, tn, fp, fn = 0, 0, 0, 0
+    for i in range(len(y_true)):
+        if y_pred[i] == y_true[i]:
+            if y_pred[i] == positive_class:
+                tp += 1
+            elif y_pred[i] == negative_class:
+                tn += 1
+        else:
+            if y_pred[i] == positive_class:
+                fp += 1
+            if y_pred[i] == negative_class:
+                fn += 1
+    return np.array([[tp, fp], [fn, tn]])
+
+def show_conf_matrix(conf_matrix: np.ndarray[2,2], lr: float, map_shape: tuple):
+    fig, ax = plt.subplots()
+    ax.matshow(conf_matrix, cmap='Blues')
+    ax.set_xlabel('Predicted', )
+    ax.set_ylabel('True')
+    ax.text(0, 0, str(conf_matrix[0][0]))
+    ax.text(1, 0, str(conf_matrix[1][0]))
+    ax.text(0, 1, str(conf_matrix[0][1]))
+    ax.text(1, 1, str(conf_matrix[1][1]))
+    # ax.set_xticks(["Positive", "Negative"])
+    # ax.set_yticks(["Negative", "Positive"])
+    ax.set_title(f'lr: {lr}, map shape: {map_shape}')
+    fig.show()
+
+def accuracy(pred: np.array, test: np.array) -> float:
+    """
+    Calculates accuracy of labels predictions: number of predicted labels
+    consistent with true labels. Prints results in percentage format.
+    :param test: True data labels
+    :param pred: Predicted data labels
+    :return: Percentage of correct predictions
+    """
+    a = 0
+    for i in range(len(pred)):
+        if pred[i] == test[i]:
+            a += 1
+    a = a / len(pred) * 100
+    # print("Accuracy: {:.2f}%".format(a))
+    return a
+
+def sensitivity_specificity(test: np.array, pred: np.array) -> tuple:
+    """
+    Calculates sensitivity and specificity of given predicted labels based on true labels.
+    In true data: 1 - healthy controls (negative), 2 - patients (positive).
+    Prints results in percentage format.
+    :param test: True data labels
+    :param pred: Predicted data labels
+    :return: Percentage sensitivity and specificity
+    """
+    # test_bin = (np.array(test) == 1).astype(int)
+    # pred_bin = (np.array(pred) == 1).astype(int)
+
+    tp, fp, fn, tn = confusion_matrix(test, pred).ravel()
+
+    sensitivity = tp / (tp + fn) * 100
+    specificity = tn / (tn + fp) * 100
+
+    print("Sensitivity: {:.2f}%".format(sensitivity))
+    print("Specificity: {:.2f}%\n".format(specificity))
+    return sensitivity, specificity
